901851937bcaf38d96c50eab41f1216ec0b35e24
Tom Sharkey <tbsharkey@gmail.com>
2019-07-24 03:53:21 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,9 +5,8 @@ def turn_count(board)
         counter += 1
       end
     end
-    return counter
+        counter
   end
-
 
   def current_player(board)
     turn_count(board) % 2 == 0 ? "X" : "O"
